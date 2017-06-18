afaaa6c2073d58827c8085dd00caf784cde4099c
MarjanaKarzek <marjana.karzek@outlook.com>
2017-06-18 14:54:29 +0200

    [#11] feat (*) -> configure view pager layout to hold fragments

--- ChiBaApp/app/src/main/java/de/emm/teama/chibaapp/Main/MainActivity.java
+++ ChiBaApp/app/src/main/java/de/emm/teama/chibaapp/Main/MainActivity.java
@@ -6,54 +6,59 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import de.emm.teama.chibaapp.Utils.BottomNavigationViewHelper;
 import de.emm.teama.chibaapp.R;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    //private static final int ACTIVITY_NUM = 0;
+    // private static final int ACTIVITY_NUM = 0;
+    private SectionsPagerAdapter adapter;
+    private ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"onCreate: starting.");
-        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
 
-        setupBottomNavigationView();
+        //Setup View Pager
+        adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        viewPager = (ViewPager) findViewById(R.id.container);
         setupViewPager(viewPager);
-    }
-
-    /**
-     * ViewPager setup
-     */
-    private void setupViewPager(ViewPager viewPager){
-        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new MainFragment());
-        adapter.addFragment(new CalendarFragment());
-        adapter.addFragment(new ToDoListFragment());
-        adapter.addFragment(new SettingsFragment());
-        viewPager.setAdapter(adapter);
-
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_home);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_calendar);
         tabLayout.getTabAt(2).setIcon(R.drawable.ic_todolist);
         tabLayout.getTabAt(3).setIcon(R.drawable.ic_settings);
+
+        setupBottomNavigationView();
     }
 
     /**
-     * BottomNavgationView setup
+     * ViewPager setup
+     * Adds the fragments: Main, Calendar, ToDoList and Settings
+     */
+    private void setupViewPager(ViewPager viewPager){adapter.addFragment(new MainFragment());
+        adapter.addFragment(new CalendarFragment());
+        adapter.addFragment(new ToDoListFragment());
+        adapter.addFragment(new SettingsFragment());
+        viewPager.setAdapter(adapter);
+    }
+
+    /**
+     * BottomNavigationView setup
      */
     private void setupBottomNavigationView(){
         Log.d(TAG,"setupBottomNavigationView: setting up BottomNavigationView");
         BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottomNavViewBar);
         BottomNavigationViewHelper.enableNavigation(MainActivity.this, bottomNavigationView);
-        //Menu menu = bottomNavigationView.getMenu();
-        //MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
-        //menuItem.setChecked(true);
-
+       // Menu menu = bottomNavigationView.getMenu();
+        //MenuItem menuItem = menu.getItem(0);
+        //menuItem.setChecked(false);
     }
 }
