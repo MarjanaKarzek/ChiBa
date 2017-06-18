08b942f08c6349cc3f5129fd206b75701b7a71d9
trevelyan@outlook.de <trevelyan@outlook.de>
2017-06-18 18:06:39 +0200

    [#4] feat (ChiBaUnity) -> add cardbard and project base

---
+++ ChiBaUnity/Assets/Scripts/FadePreloader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadePreloader : MonoBehaviour {
+
+  
+    private float loadTime;
+    private float minimumLogoTime = 5.0f;
+    private CanvasGroup fadeGroup;
+
+    private void Start()
+    {
+    
+    fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        // Start with a white screen;
+        fadeGroup.alpha = 1;
+
+        // Pre load the next scene
+        // ---
+
+        // Show Logo for minimum time
+        if (Time.time < minimumLogoTime)
+            loadTime = minimumLogoTime;
+        else
+            loadTime = Time.time;
+    }
+
+    private void Update()
+    {
+        // Fade-in
+        if(Time.time < minimumLogoTime)
+        {
+            fadeGroup.alpha = 1 - Time.time;
+        }
+
+        // Fade-out
+        if(Time.time > minimumLogoTime && loadTime != 0)
+        {
+            fadeGroup.alpha = Time.time - minimumLogoTime;
+            if(fadeGroup.alpha >=1)
+            {
+                SceneManager.LoadScene("Main_Scene");
+            }
+        }
+    }
+}
