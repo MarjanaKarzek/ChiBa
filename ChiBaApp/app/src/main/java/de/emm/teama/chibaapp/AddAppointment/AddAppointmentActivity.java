ffd1f48ff06bbeb2b982ab51155ef975604dd1ee
MarjanaKarzek <marjana.karzek@outlook.com>
2017-06-20 13:19:44 +0200

    [#25] feat (*) -> add basic form fields

--- ChiBaApp/app/src/main/java/de/emm/teama/chibaapp/AddAppointment/AddAppointmentActivity.java
+++ ChiBaApp/app/src/main/java/de/emm/teama/chibaapp/AddAppointment/AddAppointmentActivity.java
@@ -1,10 +1,19 @@
 package de.emm.teama.chibaapp.AddAppointment;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
+import de.emm.teama.chibaapp.Main.MainActivity;
 import de.emm.teama.chibaapp.R;
 
 /**
@@ -13,6 +22,7 @@ import de.emm.teama.chibaapp.R;
 
 public class AddAppointmentActivity extends AppCompatActivity{
     private static final String TAG = "AddAppointmentActivity";
+    private Context context = AddAppointmentActivity.this;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,5 +31,28 @@ public class AddAppointmentActivity extends AppCompatActivity{
         Log.d(TAG, "onCreate: started");
     }
 
+    private void setupToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.addAppointmentToolbar);
+        setSupportActionBar(toolbar);
 
+        ImageView cancel = (ImageView) findViewById(R.id.addAppointmentCancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: cancel add appointment");
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        
+        ImageView check = (ImageView) findViewById(R.id.addAppointmentOption);
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: add appointment option selected");
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
