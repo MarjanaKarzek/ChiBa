e79693ccd7524ca2456a33132bf3a1af7f832b5b
MarjanaKarzek <marjana.karzek@outlook.com>
2017-06-21 11:39:20 +0200

    [#26] feat (*) -> add form to add todo activity

--- ChiBaApp/app/src/main/java/de/emm/teama/chibaapp/AddToDo/AddToDoActivity.java
+++ ChiBaApp/app/src/main/java/de/emm/teama/chibaapp/AddToDo/AddToDoActivity.java
@@ -1,10 +1,16 @@
 package de.emm.teama.chibaapp.AddToDo;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
+import de.emm.teama.chibaapp.Main.MainActivity;
 import de.emm.teama.chibaapp.R;
 
 /**
@@ -13,11 +19,39 @@ import de.emm.teama.chibaapp.R;
 
 public class AddToDoActivity extends AppCompatActivity{
     private static final String TAG = "AddToDoActivity";
+    private Context context = AddToDoActivity.this;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_todo);
         Log.d(TAG, "onCreate: started");
+
+        setupToolbar();
+    }
+
+    private void setupToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.addToDoToolbar);
+        setSupportActionBar(toolbar);
+
+        ImageView cancel = (ImageView) findViewById(R.id.addToDoCancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: cancel add appointment");
+                Intent intent = new Intent(context, MainActivity.class);
+                context.startActivity(intent);
+            }
+        });
+
+        ImageView check = (ImageView) findViewById(R.id.addToDoOption);
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: add appointment option selected");
+                Intent intent = new Intent(context, MainActivity.class);
+                context.startActivity(intent);
+            }
+        });
     }
 }
