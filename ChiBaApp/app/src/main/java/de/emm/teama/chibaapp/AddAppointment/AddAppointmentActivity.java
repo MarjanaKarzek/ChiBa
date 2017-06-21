5356154a71c1042509a21f51008e9a456bcc2f4a
MarjanaKarzek <marjana.karzek@outlook.com>
2017-06-21 10:19:21 +0200

    [#25] feat (*) -> add date and time picker

--- ChiBaApp/app/src/main/java/de/emm/teama/chibaapp/AddAppointment/AddAppointmentActivity.java
+++ ChiBaApp/app/src/main/java/de/emm/teama/chibaapp/AddAppointment/AddAppointmentActivity.java
@@ -1,5 +1,7 @@
 package de.emm.teama.chibaapp.AddAppointment;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
@@ -11,7 +13,19 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.ViewFlipper;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import de.emm.teama.chibaapp.Main.MainActivity;
 import de.emm.teama.chibaapp.R;
@@ -23,12 +37,82 @@ import de.emm.teama.chibaapp.R;
 public class AddAppointmentActivity extends AppCompatActivity{
     private static final String TAG = "AddAppointmentActivity";
     private Context context = AddAppointmentActivity.this;
+    private Calendar calendar = Calendar.getInstance();
+    private TextView startDate;
+    private TextView endDate;
+    private TextView startTime;
+    private TextView endTime;
+    private DatePickerDialog.OnDateSetListener startDatePicker;
+    private DatePickerDialog.OnDateSetListener endDatePicker;
+    private TimePickerDialog.OnTimeSetListener startTimePicker;
+    private TimePickerDialog.OnTimeSetListener endTimePicker;
+    private String dateFormat = "d. MMMM yyyy";
+    private String timeFormat = "kk:mm";
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat, Locale.GERMANY);
+    private SimpleDateFormat simpleTimeFormat = new SimpleDateFormat(timeFormat, Locale.GERMANY);
+    private ViewFlipper viewFlipperFullDay;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_appointment);
         Log.d(TAG, "onCreate: started");
+
+        startDate = (TextView) findViewById(R.id.addAppointmentTextViewStartDate);
+        endDate = (TextView) findViewById(R.id.addAppointmentTextViewEndDate);
+        startTime = (TextView) findViewById(R.id.addAppointmentTextViewStartTime);
+        endTime = (TextView) findViewById(R.id.addAppointmentTextViewEndTime);
+        viewFlipperFullDay = (ViewFlipper) findViewById( R.id.addAppointmentViewFlipperFullDay );
+
+
+        setupDateTimePickers();
+        setupPickersOnClickListener();
+
+        Switch fulldaySwitch = (Switch) findViewById(R.id.addAppointmentSwitchFullday);
+        fulldaySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    viewFlipperFullDay.showNext();
+                }
+                else{
+                    viewFlipperFullDay.showPrevious();
+                }
+            }
+        });
+
+    }
+
+    private void setupPickersOnClickListener() {
+        startDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(AddAppointmentActivity.this, startDatePicker, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        endDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(AddAppointmentActivity.this, endDatePicker, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        startTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TimePickerDialog(AddAppointmentActivity.this, startTimePicker, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show();
+            }
+        });
+
+        endTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TimePickerDialog(AddAppointmentActivity.this, endTimePicker, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show();
+            }
+        });
     }
 
     private void setupToolbar(){
@@ -41,7 +125,7 @@ public class AddAppointmentActivity extends AppCompatActivity{
             public void onClick(View view) {
                 Log.d(TAG, "onClick: cancel add appointment");
                 Intent intent = new Intent(context, MainActivity.class);
-                startActivity(intent);
+                context.startActivity(intent);
             }
         });
         
@@ -51,8 +135,52 @@ public class AddAppointmentActivity extends AppCompatActivity{
             public void onClick(View view) {
                 Log.d(TAG, "onClick: add appointment option selected");
                 Intent intent = new Intent(context, MainActivity.class);
-                startActivity(intent);
+                context.startActivity(intent);
             }
         });
+    }
+
+    private void setupDateTimePickers(){
+        startDatePicker = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, monthOfYear);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                startDate.setText(simpleDateFormat.format(calendar.getTime()));
+            }
+        };
+
+        endDatePicker = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, monthOfYear);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                endDate.setText(simpleDateFormat.format(calendar.getTime()));
+            }
+        };
+
+        startTimePicker = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendar.set(Calendar.MINUTE, minute);
+                startTime.setText(simpleTimeFormat.format(calendar.getTime()));
+            }
+        };
+
+        endTimePicker = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendar.set(Calendar.MINUTE, minute);
+                endTime.setText(simpleTimeFormat.format(calendar.getTime()));
+            }
+        };
     }
 }
