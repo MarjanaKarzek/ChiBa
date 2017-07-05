92178a45f13cf4a93e598a66fa79b3ce1ac5d744
MarjanaKarzek <marjana.karzek@outlook.com>
2017-07-05 13:42:02 +0200

    [#56] fix (ToDoListAdapter.java) -> use view holder to make strike through of todos work

--- ChiBaApp/app/src/main/java/de/emm/teama/chibaapp/Utils/ToDoListAdapter.java
+++ ChiBaApp/app/src/main/java/de/emm/teama/chibaapp/Utils/ToDoListAdapter.java
@@ -2,11 +2,11 @@ package de.emm.teama.chibaapp.Utils;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Paint;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,7 +19,6 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 import de.emm.teama.chibaapp.R;
-import de.emm.teama.chibaapp.ToDo.EditToDoActivity;
 
 import static de.emm.teama.chibaapp.Application.ChiBaApplication.database;
 
@@ -27,14 +26,19 @@ import static de.emm.teama.chibaapp.Application.ChiBaApplication.database;
  * Created by Marjana Karzek on 22.06.2017.
  */
 
-public class ToDoListAdapter extends ArrayAdapter<Integer>{
+public class ToDoListAdapter extends ArrayAdapter<Integer> {
     private static final String TAG = "ToDoListAdapter";
     private Context context;
     private int resource;
 
-    private TextView todoText;
+    //private TextView todoText;
     private String stateString;
     private boolean state;
+
+    static class ViewHolderItem {
+        TextView todoText;
+        CheckBox checkBox;
+    }
 
     public ToDoListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull ArrayList<Integer> objects) {
         super(context, resource, objects);
@@ -45,69 +49,55 @@ public class ToDoListAdapter extends ArrayAdapter<Integer>{
     @NonNull
     @Override
     public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        final ViewHolderItem viewHolder;
+
+        if (convertView == null) {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            convertView = inflater.inflate(resource, parent, false);
+
+            viewHolder = new ViewHolderItem();
+            viewHolder.todoText = (TextView) convertView.findViewById(R.id.textViewToDoList);
+            viewHolder.checkBox = (CheckBox) convertView.findViewById(R.id.checkBoxToDoList);
+
+            viewHolder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        Log.d(TAG, "onCheckedChanged: checking text");
+                        viewHolder.todoText.setPaintFlags(viewHolder.todoText.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                        database.setStateOfToDoByToDoId(getItem(position), true);
+                        stateString = "true";
+                        state = true;
+                    } else {
+                        Log.d(TAG, "onCheckedChanged: unchecking text");
+                        viewHolder.todoText.setPaintFlags(viewHolder.todoText.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+                        database.setStateOfToDoByToDoId(getItem(position), false);
+                        stateString = "false";
+                        state = false;
+                    }
+                }
+            });
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolderItem) convertView.getTag();
+        }
 
         String titleString = database.getToDoTitleById(getItem(position));
         String durationString = database.getToDoDurationById(getItem(position));
         String toDoInformation = titleString + " (" + durationString + " Stunden)";
 
         stateString = database.getToDoStateById(getItem(position));
-        state = false;
-        Log.d(TAG, "getView: stateString is of value " + stateString);
-        if(stateString.contains("true")){
+        if (stateString.contains("true") || stateString.contains("1")) {
             state = true;
-        }
-        else
+        } else
             state = false;
-        Log.d(TAG, "getView: state is of value " + state);
 
-
-        final LayoutInflater inflater = LayoutInflater.from(context);
-        convertView = inflater.inflate(resource, parent, false);
-
-        todoText = (TextView) convertView.findViewById(R.id.textViewToDoList);
-        todoText.setText(toDoInformation);
-        CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.checkBoxToDoList);
-        checkBox.setChecked(state);
-        if(checkBox.isChecked()){
-            todoText.setPaintFlags(todoText.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        viewHolder.todoText.setText(toDoInformation);
+        viewHolder.checkBox.setChecked(state);
+        if (viewHolder.checkBox.isChecked()) {
+            viewHolder.todoText.setPaintFlags(viewHolder.todoText.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
         }
-
-        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    Log.d(TAG, "onCheckedChanged: checking text");
-                    todoText.setPaintFlags(todoText.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-                    database.setStateOfToDoByToDoId(getItem(position), true);
-                    stateString = "true";
-                    state = true;
-                    synchronized (todoText){
-                        todoText.notifyAll();
-                    }
-                }
-                else {
-                    Log.d(TAG, "onCheckedChanged: unchecking text");
-                    todoText.setPaintFlags(todoText.getPaintFlags() & (~ Paint.STRIKE_THRU_TEXT_FLAG));
-                    database.setStateOfToDoByToDoId(getItem(position), false);
-                    stateString = "false";
-                    state = false;
-                    synchronized (todoText){
-                        todoText.notifyAll();
-                    }
-                }
-            }
-        });
-
-        /*todoText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onItemClick: clicked");
-                Activity activity = (Activity) context;
-                Intent intent = new Intent(activity, EditToDoActivity.class);
-                intent.putExtra("EXTRA_TODO_ID", getItem(position));
-                activity.startActivity(intent);
-            }
-        });*/
 
         return convertView;
     }
