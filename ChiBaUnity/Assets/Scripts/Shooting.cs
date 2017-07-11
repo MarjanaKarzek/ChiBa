c370b639a20f9bd8f81377f192e43d9bd1db1370
Jhomar Besmens <jhomar.besmens@outlook.de>
2017-07-11 14:14:26 +0200

    [#47] feat (ChibaUnity/Assets/Scripts) -> add scripts for inventory

---
+++ ChiBaUnity/Assets/Scripts/Shooting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    public InventoryItem projectileInventoryItem;
+    private Inventory inventory;
+
+    public GameObject prefabObject;
+
+    public void Shoot()
+    {
+        inventory = transform.parent.GetComponent<Inventory>();
+
+        if (inventory.RemoveItem(projectileInventoryItem))
+        {
+            GameObject obj = Instantiate(projectileInventoryItem.prefab, transform.position, transform. rotation);
+            obj.transform.position = new Vector3(Random.Range(-200.0f, 200.0f), 700, Random.Range(-200.0f, 200.0f));
+        }
+    }
+}
---
+++ ChiBaUnity/Assets/Scripts/ThrowBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBehaviour : MonoBehaviour {
+
+    public float speed = 10;
+
+    void FixedUpdate()
+    {
+        transform.Translate(transform.forward * speed * Time.deltaTime, Space.World);   
+    }
+
+    
+}
