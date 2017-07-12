a3fbe6e085bd43f41e0c8e223fa1a09cd9da87c8
MarjanaKarzek <marjana.karzek@outlook.com>
2017-07-12 12:12:36 +0200

    [#70] feat (ChiBaApp) -> add layout for city change dialog

---
+++ ChiBaApp/app/src/main/java/de/emm/teama/chibaapp/Utils/CityChangeDialogFragment.java
@@ -0,0 +1,13 @@
+package de.emm.teama.chibaapp.Utils;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by Marjana Karzek on 12.07.2017.
+ */
+
+public class CityChangeDialogFragment extends DialogFragment{
+    private static final String TAG = "CityChangeDialogFragmen";
+
+
+}
