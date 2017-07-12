f50906a161b0f8f20ea76e1800f8c0dec5ff0546
MarjanaKarzek <marjana.karzek@outlook.com>
2017-07-12 12:42:38 +0200

    [#63] refactor (CalendarFragment.java) -> use view holder to increase performance

--- ChiBaApp/app/src/main/java/de/emm/teama/chibaapp/Main/CalendarFragment.java
+++ ChiBaApp/app/src/main/java/de/emm/teama/chibaapp/Main/CalendarFragment.java
@@ -35,57 +35,64 @@ import static de.emm.teama.chibaapp.Application.ChiBaApplication.database;
 
 public class CalendarFragment extends Fragment {
     private static final String TAG = "CalendarFragment";
+
     private ArrayList<String> currentEvents = new ArrayList<String>();
+    private ArrayList<Integer> displayedEvents = new ArrayList<Integer>();
     private DisplayEventListAdapter adapter;
-    private TextView selectedDate;
+
     private String dateFormat = "d. MMMM yyyy";
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat, Locale.GERMANY);
-    private CalendarView calendarView;
     private Calendar calendar = Calendar.getInstance();
-    private ArrayList<Integer> displayedEvents = new ArrayList<Integer>();
 
-    private ListView eventlist;
+    private View view;
+    private final ViewHolderItem viewHolder = new ViewHolderItem();
+
+    static class ViewHolderItem {
+        ListView eventlist;
+        TextView selectedDate;
+        CalendarView calendarView;
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
-
-        eventlist = (ListView) view.findViewById(R.id.eventlist);
-        eventlist.setEmptyView(view.findViewById(R.id.textViewEventsEmpty));
-        selectedDate = (TextView) view.findViewById(R.id.selectedDate);
-        calendarView = (CalendarView) view.findViewById(R.id.calendarView);
-        selectedDate.setText(simpleDateFormat.format(calendarView.getDate()));
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                calendar.set(Calendar.YEAR, year);
-                calendar.set(Calendar.MONTH, month);
-                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                selectedDate.setText(simpleDateFormat.format(calendar.getTime()));
-                updateEventList();
-            }
-        });
-        eventlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                Fragment prev = getFragmentManager().findFragmentByTag("appointment_detail_fragment");
-                if (prev != null) {
-                    transaction.remove(prev);
+        if (view == null) {
+            view = inflater.inflate(R.layout.fragment_calendar, container, false);
+            viewHolder.eventlist = (ListView) view.findViewById(R.id.eventlist);
+            viewHolder.eventlist.setEmptyView(view.findViewById(R.id.textViewEventsEmpty));
+            viewHolder.selectedDate = (TextView) view.findViewById(R.id.selectedDate);
+            viewHolder.calendarView = (CalendarView) view.findViewById(R.id.calendarView);
+            viewHolder.selectedDate.setText(simpleDateFormat.format(viewHolder.calendarView.getDate()));
+            viewHolder.calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+                @Override
+                public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                    calendar.set(Calendar.YEAR, year);
+                    calendar.set(Calendar.MONTH, month);
+                    calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                    viewHolder.selectedDate.setText(simpleDateFormat.format(calendar.getTime()));
+                    updateEventList();
                 }
-                transaction.addToBackStack(null);
+            });
+            viewHolder.eventlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                    Fragment prev = getFragmentManager().findFragmentByTag("appointment_detail_fragment");
+                    if (prev != null) {
+                        transaction.remove(prev);
+                    }
+                    transaction.addToBackStack(null);
 
-                // Create and show the dialog.
-                DialogFragment newDialog = AppointmentDetailDialogFragment.newInstance(displayedEvents.get(position));
-                newDialog.show(transaction, "appointment_detail_fragment");
-            }
-        });
+                    // Create and show the dialog.
+                    DialogFragment newDialog = AppointmentDetailDialogFragment.newInstance(displayedEvents.get(position));
+                    newDialog.show(transaction, "appointment_detail_fragment");
+                }
+            });
 
-        displayData();
-        adapter = new DisplayEventListAdapter(inflater.getContext(), R.layout.layout_list_events_adapter_view, currentEvents);
-        eventlist.setAdapter(adapter);
-
+            displayData();
+            adapter = new DisplayEventListAdapter(inflater.getContext(), R.layout.layout_list_events_adapter_view, currentEvents);
+            viewHolder.eventlist.setAdapter(adapter);
+        }
         return view;
     }
 
@@ -93,14 +100,14 @@ public class CalendarFragment extends Fragment {
         displayedEvents.clear();
         currentEvents.clear();
         displayData();
-        synchronized (adapter){
+        synchronized (adapter) {
             adapter.notifyDataSetChanged();
         }
 
     }
 
     private void displayData() {
-        Cursor data = database.showEventsByStartDateWithoutFullDay(selectedDate.getText().toString());
+        Cursor data = database.showEventsByStartDateWithoutFullDay(viewHolder.selectedDate.getText().toString());
         displayedEvents.clear();
         currentEvents.clear();
 
@@ -115,13 +122,13 @@ public class CalendarFragment extends Fragment {
             }
         }
 
-        Cursor dataFullday = database.showEventIdsByStartDateThatAreFullDay(selectedDate.getText().toString());
+        Cursor dataFullday = database.showEventIdsByStartDateThatAreFullDay(viewHolder.selectedDate.getText().toString());
 
         if (dataFullday.getCount() != 0) {
             String event;
             while (dataFullday.moveToNext()) {
                 Cursor currentFulldayEvent = database.showEventByEventId(Integer.valueOf(dataFullday.getString(0)));
-                if(currentFulldayEvent.getCount() != 0 && currentFulldayEvent.moveToNext()){
+                if (currentFulldayEvent.getCount() != 0 && currentFulldayEvent.moveToNext()) {
                     event = currentFulldayEvent.getString(1);
                     displayedEvents.add(Integer.valueOf(currentFulldayEvent.getString(0)));
                     currentEvents.add(event);
