f7a278ccd3fb7231597c3a2108ce3106370a4b7f
JhomarBesmens <trevelyan@outlook.de>
2017-07-12 23:05:20 +0200

    [#76] feat (ChibaUnity/Assets/Materials) -> add animation controller script

---
+++ ChiBaUnity/Assets/Scripts/ChiBaAnimator.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChiBaAnimator : MonoBehaviour {
+
+    private Animator anim;
+    public float speed = 2.5f;
+    bool IsMoving;
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        //IsMoving = GameObject.Find("Player").GetComponent<VRLookAndWalk>().moveForward;
+	}
+
+    void FixedUpdate()
+    {
+        IsMoving = GameObject.Find("Player").GetComponent<VRLookAndWalk>().moveForward;
+
+        if (IsMoving)
+        {
+            anim.SetBool("IsWalking", true);
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+        }
+    }
+
+
+}
