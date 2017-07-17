6e2eb9ee50ff573caa03e72696343fbd2cfcba33
JhomarBesmens <trevelyan@outlook.de>
2017-07-17 14:28:40 +0200

    [#77] feat (ChibaUnity/Assets) -> Add collisions and physic behaviour for objects in main_scene

---
+++ ChiBaUnity/Assets/Scripts/KickItems.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickItems : MonoBehaviour {
+
+    public int AddForceMulti = 20;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+     
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+
+        rb.AddForce(Vector3.up * AddForceMulti, ForceMode.Impulse);
+    }
+}
