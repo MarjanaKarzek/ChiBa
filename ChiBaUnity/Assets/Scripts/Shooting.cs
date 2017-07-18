f1f4569c011c0473f3425422cda5be166fa2a522
JhomarBesmens <trevelyan@outlook.de>
2017-07-18 08:18:38 +0200

    [#77] fix (ChibaUnity) -> change masss of ball_prefab and UI position

--- ChiBaUnity/Assets/Scripts/Shooting.cs
+++ ChiBaUnity/Assets/Scripts/Shooting.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shooting : MonoBehaviour {
 
@@ -49,5 +50,6 @@ public class Shooting : MonoBehaviour {
 
             inventory.RemoveItem(ball);
         }
+
     }
 }
