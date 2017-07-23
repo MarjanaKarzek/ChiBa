4ebbf67720525c19d320257008665e3fff5880ad
JhomarBesmens <trevelyan@outlook.de>
2017-07-24 00:21:28 +0200

    [#87] refactor (ChibaUnity) -> polish and clean up folder and files

--- ChiBaUnity/Assets/Scripts/ChibaRotate.cs
+++ ChiBaUnity/Assets/Scripts/ChibaRotate.cs
@@ -11,14 +11,14 @@ using UnityEngine;
  */
 public class ChibaRotate : MonoBehaviour {
 
-    public GameObject camera;   /*!< Variable for the camera */
+    public GameObject cameraChiba;   /*!< Variable for the camera */
     public Quaternion rotateChiba = Quaternion.identity;    /*!< Variable for the identity rotation from the chiba model in the world axes */
 
 	/**
      * This method checks if the euler angles has changed and update them to the rotation identity from the chiba model.
      */
 	void Update () {
-        rotateChiba.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, camera.transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.x);
+        rotateChiba.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, cameraChiba.transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.x);
         transform.rotation = rotateChiba;
 
     }
